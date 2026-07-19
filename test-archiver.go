20d7326d540ff062f41dbfa30791a0c9076ad089
Mauricio González Prendas <me@maugp.com>
2026-07-19 06:25:08 -0600

    chore: remove compiled binary and test script from repository

--- test-archiver.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"github.com/mholt/archiver/v3"
-)
-func main() {
-	err := archiver.Walk("test.zip", func(f archiver.File) error { return nil })
-	fmt.Println(err)
-}
